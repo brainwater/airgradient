8b56348afe0c0b0da0402485359de6f69ca96e6c
Blake Rainwater <rainwaterblake@gmail.com>
2023-08-16 11:14:54 -0400

    Update DIY Basic sketch with home assistant integration

--- examples/DIY_BASIC/DIY_BASIC.ino
+++ examples/DIY_BASIC/DIY_BASIC.ino
@@ -30,6 +30,7 @@ MIT License
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <U8g2lib.h>
+#include <ArduinoHA.h>
 
 AirGradient ag = AirGradient();
 
@@ -69,11 +70,33 @@ const int pm25Interval = 5000;
 unsigned long previousPm25 = 0;
 int pm25 = 0;
 
+const int pm1Interval = 5000;
+unsigned long previousPm1 = 0;
+int pm1 = 0;
+
 const int tempHumInterval = 2500;
 unsigned long previousTempHum = 0;
 float temp = 0;
 int hum = 0;
 long val;
+
+char mqtt_server[40] = "homeassistant.local";
+int mqtt_port = 1883;
+char mqtt_username[40] = "username";
+char mqtt_password[40] = "password";
+
+char unique[40] = "tempUnique";
+WiFiManager wifiManager;
+WiFiClient client;
+HADevice device(unique);
+HAMqtt mqtt(client, device, 7);
+
+HASensorNumber haco2("airGradientCO2");
+HASensorNumber hapms("airGradientPMS");
+HASensorNumber hatmp("airGradientTmp", HASensorNumber::PrecisionP1);
+HASensorNumber hahum("airGradientHum");
+HASensorNumber hapm25("airGradientPM25");
+HASensorNumber hapm1("airGradientPM1");
 
 void setup()
 {
@@ -81,10 +104,10 @@ void setup()
   u8g2.setBusClock(100000);
   u8g2.begin();
   updateOLED();
-
-    if (connectWIFI) {
+  if (connectWIFI) {
     connectToWifi();
   }
+  connectToMqtt();
   updateOLED2("Warm Up", "Serial#", String(ESP.getChipId(), HEX));
   ag.CO2_Init();
   ag.PMS_Init();
@@ -98,8 +121,11 @@ void loop()
   updateOLED();
   updateCo2();
   updatePm25();
+  updatePm1();
   updateTempHum();
-  sendToServer();
+  //sendToServer();
+  sendToMqtt();
+  delay(1000);
 }
 
 void updateCo2()
@@ -107,7 +133,7 @@ void updateCo2()
     if (currentMillis - previousCo2 >= co2Interval) {
       previousCo2 += co2Interval;
       Co2 = ag.getCO2_Raw();
-      Serial.println(String(Co2));
+      //Serial.println(String(Co2));
     }
 }
 
@@ -116,7 +142,16 @@ void updatePm25()
     if (currentMillis - previousPm25 >= pm25Interval) {
       previousPm25 += pm25Interval;
       pm25 = ag.getPM2_Raw();
-      Serial.println(String(pm25));
+      //Serial.println(String(pm25));
+    }
+}
+
+void updatePm1()
+{
+    if (currentMillis - previousPm1 >= pm1Interval) {
+      previousPm1 += pm1Interval;
+      pm1 = ag.getPM1_Raw();
+      Serial.println(String(pm1));
     }
 }
 
@@ -127,7 +162,7 @@ void updateTempHum()
       TMP_RH result = ag.periodicFetchData();
       temp = result.t;
       hum = result.rh;
-      Serial.println(String(temp));
+      //Serial.println(String(temp));
     }
 }
 
@@ -184,7 +219,7 @@ void sendToServer() {
         Serial.println(payload);
         String POSTURL = APIROOT + "sensors/airgradient:" + String(ESP.getChipId(), HEX) + "/measures";
         Serial.println(POSTURL);
-        WiFiClient client;
+        //WiFiClient client;
         HTTPClient http;
         http.begin(client, POSTURL);
         http.addHeader("content-type", "application/json");
@@ -202,17 +237,64 @@ void sendToServer() {
 
 // Wifi Manager
  void connectToWifi() {
-   WiFiManager wifiManager;
    //WiFi.disconnect(); //to delete previous saved hotspot
    String HOTSPOT = "AG-" + String(ESP.getChipId(), HEX);
    updateOLED2("Connect", "Wifi AG-", String(ESP.getChipId(), HEX));
    delay(2000);
    wifiManager.setTimeout(90);
+   //wifiManager.startConfigPortal();
    if (!wifiManager.autoConnect((const char * ) HOTSPOT.c_str())) {
      updateOLED2("Booting", "offline", "mode");
      Serial.println("failed to connect and hit timeout");
      delay(6000);
    }
+}
+
+void connectToMqtt() {
+  // TODO: Is there support for ssl or encryption for mqtt?
+  
+  Serial.println("Connecting to mqtt/ha");
+  strcpy(unique, String(ESP.getChipId(), HEX).c_str());
+  /*byte mac[WL_MAC_ADDR_LENGTH];
+  WiFi.macAddress(mac);
+  device.setUniqueId(mac, sizeof(mac));*/
+  device.setName("Air Gradient");
+  device.enableSharedAvailability();
+  device.enableLastWill();
+  device.setManufacturer("Air Gradient");
+  device.setModel("DIY Basic PCBv2");
+  
+  hatmp.setName("Air Gradient Temperature");
+  hahum.setName("Air Gradient Humidity");
+  haco2.setName("Air Gradient CO2");
+  hapm25.setName("Air Gradient PM2.5");
+  hapm1.setName("Air Gradient PM1.0");
+  hatmp.setDeviceClass("temperature");
+  hahum.setDeviceClass("humidity");
+  haco2.setDeviceClass("carbon_dioxide");
+  hapm25.setDeviceClass("pm25");
+  hapm1.setDeviceClass("pm1");
+  hatmp.setUnitOfMeasurement("°C");
+  hahum.setUnitOfMeasurement("%");
+  haco2.setUnitOfMeasurement("ppm");
+  //TODO: Are the pm measurement units correct? https://www.home-assistant.io/integrations/sensor/ Indicates the measurements are in µg/m³
+  hapm25.setUnitOfMeasurement("ppm");
+  hapm1.setUnitOfMeasurement("ppm");
+  mqtt.begin(mqtt_server, mqtt_port, mqtt_username, mqtt_password);
+  // Important! I think this is required to make sure the config is sent, but now it is sending the config without this!?
+  device.setAvailability(true);
+  //TODO: What happens with the config when home assistant is restarted?
+  Serial.println("Connected to mqtt/ha");
+  
+}
+
+void sendToMqtt() {
+  mqtt.loop();
+  hatmp.setValue(temp);
+  hahum.setValue(hum);
+  haco2.setValue(Co2);
+  hapm25.setValue(pm25);
+  hapm1.setValue(pm1);
 }
 
 // Calculate PM2.5 US AQI
